1a314a2db8b0f08db8e0ffa70b1df184d3e76938
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-15 15:18:36 +0100

    feat: definizione costruttore

--- index.php
+++ index.php
@@ -8,4 +8,13 @@ class Movie
     public $durata;
     public $genere;
     public $anno;
+
+    // definizione costruttore
+    function __construct($nomeFilm, $durataFilm, $genereFilm, $annoFilm)
+    {
+        $this->nome = $nomeFilm;
+        $this->durata = $durataFilm;
+        $this->genere = $genereFilm;
+        $this->anno = $annoFilm;
+    }
 }
