72dee9aa2de88a04ec7bd4020f9e755e9a9eaf11
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-15 16:06:26 +0100

    feat: istanziati due oggetti e stampa in pagina dei valori

--- index.php
+++ index.php
@@ -28,3 +28,27 @@ class Movie
         };
     }
 };
+
+
+// istanza di due oggetti
+$unaNotteDaLeoni = new Movie("Una Notte Da Leoni", 1.39, "Commedia", 2009);
+$unaNotteDaLeoniDue = new Movie("Una Notte Da Leoni Due", 1.41, "Commedia", 2011);
+
+// stampa in pagina
+// una notte da leoni
+echo "Il titolo del film è: " . $unaNotteDaLeoni->nome;
+echo "<br>";
+echo "La durata arrotondata del film è: " . $unaNotteDaLeoni->durata;
+echo "<br>";
+echo "Il genere del film è: " . $unaNotteDaLeoni->genere;
+echo "<br>";
+echo "L'anno di pubblicazione del film è: " . $unaNotteDaLeoni->anno;
+echo "<hr>";
+// una notte da leoni due
+echo "Il titolo del film è: " . $unaNotteDaLeoniDue->nome;
+echo "<br>";
+echo "La durata arrotondata del film è: " . $unaNotteDaLeoniDue->durata;
+echo "<br>";
+echo "Il genere del film è: " . $unaNotteDaLeoniDue->genere;
+echo "<br>";
+echo "L'anno di pubblicazione del film è: " . $unaNotteDaLeoniDue->anno;
